28d333ecd4b589e0ede353a1b016fd44b0eecfce
Ashik Meher Mobin <mobinashikmeher>
2021-02-07 10:41:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+/*
+var div = document.createElement("div");
+div.style.width = "200px";
+div.style.height = "auto";
+div.style.background = "white";
+div.style.overflow = "hidden";
+div.style.color = "black";
+div.style.fontSize = "26px";
+div.style.borderRadius = "10px";
+div.style.padding = "9px";
+div.style.display = "flex";
+div.style.justifyContent = "center";
+div.style.margin = "auto";
+div.style.marginTop = "50px";
+
+
+div.innerHTML = "How are you? you have been hacked!";
+
+
+var btn = "<button> Safeguard me! </button>";
+
+
+
+
+var ctn = document.getElementById("btnc");
+ctn.addEventListener("click", function () { alert("Hello World!"); ctn.style.display = "none"; });
+
+
+
+
+
+
+
+
+
+//Joining html elements
+div.innerHTML += btn;
+
+
+document.getElementById("wrapper").appendChild(div);
+
+//
+//var wrap = document.getElementById("wrapper");
+//wrap.style.display = "flex";
+//wrap.style.justifyContent = "center";
+*/
